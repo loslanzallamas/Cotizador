aef3381aee0318632dc73043bab50a03995504ad
Administrador <Administrador@192.168.1.152>
2012-09-19 16:57:04 -0300

    Add Class

---
+++ SpringInAction/src/main/java/com/springinactions/springidol/Piano.java
@@ -0,0 +1,15 @@
+package com.springinactions.springidol;
+
+public class Piano implements Instrument {
+
+	public Piano() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void play() {
+		
+		System.out.println("PLINK PLINK PLINK");
+
+	}
+
+}
---
+++ SpringInAction/src/main/java/com/springinactions/springidol/Saxophone.java
@@ -0,0 +1,13 @@
+package com.springinactions.springidol;
+
+public class Saxophone implements Instrument {
+
+	public Saxophone() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void play() {
+		System.out.println("TOOT TOOT TOOT");
+	}
+
+}
